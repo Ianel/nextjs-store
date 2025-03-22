9d65860d6e49ebd53fb36bcdfabc386361a42b57
Ianel Tombozafy <ianell.s118@gmail.com>
2025-03-22 09:12:37 +0300

    feat: design product details modal

--- src/components/common/ProductCard.tsx
+++ src/components/common/ProductCard.tsx
@@ -1,17 +1,47 @@
 import { Card, CardContent } from "@/components/ui/card";
 import Image from "next/image";
+import {
+    Dialog,
+    DialogContent,
+    DialogHeader,
+    DialogTitle,
+    DialogTrigger,
+} from "@/components/ui/dialog";
 import { Button } from "../ui/button";
 
 export default function ProductCard() {
     return (
         <Card className="md:w-72 p-0 relative">
-            <Image
-                width={400}
-                height={200}
-                src="https://picsum.photos/400/200"
-                alt="Dummy image"
-                className="rounded-t-xl"
-            />
+            <Dialog>
+                <DialogTrigger asChild>
+                    <Image
+                        width={400}
+                        height={200}
+                        src="https://picsum.photos/400/200"
+                        alt="Dummy image"
+                        className="rounded-t-xl cursor-pointer"
+                    />
+                </DialogTrigger>
+                <DialogContent className="sm:max-w-[425px]">
+                    <DialogHeader>
+                        <DialogTitle>Air Jordan</DialogTitle>
+                    </DialogHeader>
+
+                    <section className="flex flex-col gap-1.5">
+                        <h3 className="text-slate-500 font-semibold">
+                            $100.20
+                        </h3>
+                        <p>
+                            Lorem ipsum dolor sit amet consectetur adipisicing
+                            elit. Eius optio soluta expedita, excepturi quisquam
+                            officia praesentium. Dolorum suscipit necessitatibus
+                            nisi molestiae, mollitia iusto officia provident
+                            dolores blanditiis perspiciatis delectus cumque!
+                        </p>
+                    </section>
+                </DialogContent>
+            </Dialog>
+
             <div className="absolute top-4 right-4 bg-chart-2 text-white px-3 py-1 rounded-full text-sm font-medium">
                 Popular
             </div>
